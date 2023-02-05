6249678fc468adab91842bda0d5cd5ff74ba3ffe
VJ-144 <VijayChand144@Gmail.com>
2023-02-05 22:57:19 +0000

    Updated for new data format

--- PlotObservables.py
+++ PlotObservables.py
@@ -5,51 +5,71 @@ import matplotlib.pyplot as plt
 
 def main():
 
-    # file with Susceptibility and Heat Capacity Data
-    file = '50N_Temp1.0_GlauberModel.dat'
+    # model = 'Kawasaki'
+    model = 'Glauber'
 
-    # selects the correct directiory the data is in based off file name
-    if (len(file) > 28):
-        model = 'Kawasaki'
-    else:
-        model = 'Glauber'    
-
-    # finds data from correct directories
-    # pathToFile = f'C:\\Users\\Vijay\\OneDrive\\Documents\\Univeristy Work\\Year 5\\MVP\\checkpoint1\\Data\\{model}\\'
     pathToFile = os.getcwd() + f'\\Data\\{model}\\'
-    Data = np.loadtxt(pathToFile + file)
+    directory = os.fsencode(pathToFile)
 
-    # Susceptibility and Heat Capacity Data
-    suscept = Data[:,0]
-    heat_cap = Data[:,1]
-    energy = Data[:,2]
-    magnetism = Data[:,3]
+    tot_energy = []
+    tot_mag = []
+    tot_hCap = []
+    tot_sus = []
+
+    tot_kT = []
+        
+    for file in os.listdir(directory):
+
+        filename = os.fsdecode(file)
+        directory = os.fsdecode(directory)
+        path = os.path.join(directory, filename)
+
+        kT = float(filename[8:11])
+        N = int(filename[0:2])
+
+        rawData = np.loadtxt(path)
+        energy = rawData[:,0]
+        mag = rawData[:,1]
+
+        aver_En = np.mean(energy)
+        aver_Mag = np.mean(mag)
+
+        h_capac = (1/(N*N*kT*kT)) * np.var(energy)
+        suscept = (1/(N*N*kT)) * np.var(mag)
+
+
+        tot_energy.append(aver_En)
+        tot_mag.append(aver_Mag)
+        tot_hCap.append(h_capac)
+        tot_sus.append(suscept)
+        tot_kT.append(kT)
+
 
     # setting canvas for plotting
     fig, ax = plt.subplots(2, 2, figsize=(7, 5))
 
     # setting figure title
-    fig.suptitle(f'{file}', fontsize=16)
+    fig.suptitle(f'{filename}', fontsize=16)
     fig.subplots_adjust(top=0.8, hspace=0.4)
 
     # plotting susceptibility + title
     ax[0,0].set_title('Susceptibility', pad=16)
-    ax[0,0].plot(suscept)
+    ax[0,0].plot(tot_sus, tot_kT)
     # ax[0,0].ticklabel_format(axis='x', style='sci', scilimits=(4,4))
 
     # plotting heat capacity + title
     ax[0,1].set_title('Heat Capacity', pad=16)
-    ax[0,1].plot(heat_cap)
+    ax[0,1].plot(tot_hCap)
     # ax[0,1].ticklabel_format(axis='both', style='sci', scilimits=(4,4))
 
     # plotting magnetism + title
     ax[1,0].set_title('Magnetism', pad=4)
-    ax[1,0].plot(magnetism)
+    ax[1,0].plot(tot_mag)
     # ax[1,0].ticklabel_format(axis='both', style='sci', scilimits=(4,4))
 
     # plotting energy + title
     ax[1,1].set_title('Energy', pad=4)
-    ax[1,1].plot(energy)
+    ax[1,1].plot(tot_energy)
     # ax[1,1].ticklabel_format(axis='both', style='sci', scilimits=(4,4))
 
     # displays plot
